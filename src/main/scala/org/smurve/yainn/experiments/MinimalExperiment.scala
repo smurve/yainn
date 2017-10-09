6e48b878efd2fcc3aee8948fade0740a3dfb3e76
Wolfgang Giersche <wgiersche@gmail.com>
2017-10-09 17:04:59 +0200

    bit'o clean-up

--- src/main/scala/org/smurve/yainn/experiments/MinimalExperiment.scala
+++ src/main/scala/org/smurve/yainn/experiments/MinimalExperiment.scala
@@ -14,25 +14,18 @@ object MinimalExperiment {
     val NOISE = 0.7
     val BATCH_SIZE = 20
     val NUM_BATCHES = 20
-    val ETA = 1
+    val ETA = .3
     val N_TEST = 100
 
-    val (nx, nh, ny) = (9, 16, 2)
-
-    val W0 = Nd4j.rand(nh, nx, seed) - 0.5
-    val b0 = Nd4j.rand(nh, 1, seed) - 0.5
-    val W1 = Nd4j.rand(ny, nh, seed) - 0.5
-    val b1 = Nd4j.rand(ny, 1, seed) - 0.5
-
     val data = new MinimalDataIterator(NOISE, BATCH_SIZE, seed)
-
-    val nn = Affine(W0, b0) !! Sigmoid() !! Affine(W1, b1) !! Sigmoid() !! Output(euc, euc_prime)
-
     val testSet = data.nextBatch(N_TEST)
+
+    val nn = createNetwork (seed)
 
     for ( b <- 1 to NUM_BATCHES ) {
 
       val trainingSet = data.nextBatch()
+
       val allCost = for ((symbol, label) <- trainingSet) yield {
 
         val BackPack(cost, _, grads) = nn.fbp(symbol, label)
@@ -43,13 +36,38 @@ object MinimalExperiment {
         cost
       }
       val avg = allCost.sum / BATCH_SIZE
+
       println(s"Batch Nr: $b: Cost=$avg")
       //println("Validating...")
       val successRate = validate ( nn, testSet ) * 100
       println(s"Sucess rate: $successRate")
     }
+
+
   }
 
+  /**
+    * create the network from randomly initialized weights and biases
+    * @param seed a random seed
+    * @return the first layer representing the network
+    */
+  def createNetwork (seed: Long): Layer = {
+    val (nx, nh, ny) = (9, 16, 2)
+
+    val W0 = Nd4j.rand(nh, nx, seed) - 0.5
+    val b0 = Nd4j.rand(nh, 1, seed) - 0.5
+    val W1 = Nd4j.rand(ny, nh, seed) - 0.5
+    val b1 = Nd4j.rand(ny, 1, seed) - 0.5
+
+    Affine(W0, b0) !! Sigmoid() !! Affine(W1, b1) !! Sigmoid() !! Output(euc, euc_prime)
+  }
+
+  /**
+    * Count the number of true positives within a test set.
+    */
   def validate(nn: Layer, testSet: List[(T, T)]): Double =
     testSet.map{ case (image, label) => if ( equiv(nn.fp(image), label)) 1 else 0 }.sum.toDouble / testSet.size
+
+
 }
+
